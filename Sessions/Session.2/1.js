b1f4fdcc23eff918efdec14fccf8c73381d400dc
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-08-28 15:08:39 +0900

    Session.2 1,2차원 탐색 끝

--- Sessions/Session.2/1.js
+++ Sessions/Session.2/1.js
@@ -1,5 +1,24 @@
-const solution = function(){
+const solution = function(arr){
 
-
+    let answer = [arr[0]]
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > arr[i-1]){
+            answer.push(arr[i])
+        }
+    }
+    console.log(answer)
 }
-solution()
+solution([7,3,9,5,6,12])
+
+//답지
+// function solution(arr){         
+//     let answer=[];
+//     answer.push(arr[0]);
+//     for(let i=1; i<arr.length; i++){
+//         if(arr[i]>arr[i-1]) answer.push(arr[i]);
+//     }
+//     return answer;
+// }
+
+// let arr=[7, 3, 9, 5, 6, 12];
+// console.log(solution(arr));
--- Sessions/Session.2/4.js
+++ Sessions/Session.2/4.js
@@ -1,6 +1,33 @@
-const solution = function(){
-
-
+const solution = function(arr){
+    let plus = 0
+    let answer = 0
+    for(let x of arr){
+        if(x === 0){
+            plus = 0
+            continue
+        }
+        plus++
+        answer += plus
+    }
+    console.log(answer)
 }
+let arr=[1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+solution(arr)
 
-solution()
+
+//답지
+// function solution(arr){         
+//     let answer=0, cnt=0;
+//     for(let x of arr){
+//         if(x===1){
+//             cnt++;
+//             answer+=cnt;
+//         }
+//         else cnt=0;
+//     }
+       
+//     return answer;
+// }
+
+// let arr=[1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+// console.log(solution(arr));
